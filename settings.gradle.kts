7cde9952b6c45ed4dd7f33ac2aa53bf73e9c8d71
tranthihongphuc <hongphuc0987@gmail.com>
2024-10-10 21:12:32 +0700

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "PRM_JewelryAuction_Mobile"
+include(":app")
