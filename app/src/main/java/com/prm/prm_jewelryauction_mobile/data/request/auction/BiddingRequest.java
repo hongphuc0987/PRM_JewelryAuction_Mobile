d43778c2ab937b1606fd15b0143dbd15ed129f96
tranthihongphuc <hongphuc0987@gmail.com>
2024-10-21 23:13:45 +0700

    bidding

--- app/src/main/java/com/prm/prm_jewelryauction_mobile/data/request/auction/BiddingRequest.java
+++ app/src/main/java/com/prm/prm_jewelryauction_mobile/data/request/auction/BiddingRequest.java
@@ -4,4 +4,25 @@ public class BiddingRequest {
     private long auctionId;
 
     private float bidAmount;
+
+    public BiddingRequest(long auctionId, float bidAmount) {
+        this.auctionId = auctionId;
+        this.bidAmount = bidAmount;
+    }
+
+    public long getAuctionId() {
+        return auctionId;
+    }
+
+    public void setAuctionId(long auctionId) {
+        this.auctionId = auctionId;
+    }
+
+    public float getBidAmount() {
+        return bidAmount;
+    }
+
+    public void setBidAmount(float bidAmount) {
+        this.bidAmount = bidAmount;
+    }
 }
