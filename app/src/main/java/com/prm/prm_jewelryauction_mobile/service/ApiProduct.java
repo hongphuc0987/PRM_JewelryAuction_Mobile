953c2a8c885b77f62dcb79a8566371c9e52f63ec
namhuu6969 <namquangbinh6969@gmail.com>
2024-10-24 23:09:21 +0700

    valuation dialog

--- app/src/main/java/com/prm/prm_jewelryauction_mobile/service/ApiProduct.java
+++ app/src/main/java/com/prm/prm_jewelryauction_mobile/service/ApiProduct.java
@@ -6,9 +6,12 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface ApiProduct {
     @GET("/api/v1/jewelry/me")
     Call<List<JewelryModel>> getProduct();
 
+    @GET("/api/v1/jewelry/{id}")
+    Call<JewelryModel> getProductById(@Path("id") int id);
 }
