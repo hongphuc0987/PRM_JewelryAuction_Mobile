127326c265309df7f4600c60d84a6e7fb1be8cd2
namhuu6969 <namquangbinh6969@gmail.com>
2024-10-27 16:12:11 +0700

    valuation list and modal add auction

---
+++ app/src/main/java/com/prm/prm_jewelryauction_mobile/adapter/ValuationAdapter/ValuationAdapter.java
@@ -0,0 +1,76 @@
+package com.prm.prm_jewelryauction_mobile.adapter.ValuationAdapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.prm.prm_jewelryauction_mobile.R;
+import com.prm.prm_jewelryauction_mobile.activity.auction.AuctionDetailActivity;
+import com.prm.prm_jewelryauction_mobile.activity.product_management.DetailActivity;
+import com.prm.prm_jewelryauction_mobile.activity.product_management.DetailValuating;
+import com.prm.prm_jewelryauction_mobile.model.JewelryModel;
+import com.prm.prm_jewelryauction_mobile.model.Product;
+import com.prm.prm_jewelryauction_mobile.model.Valuation;
+
+import java.util.List;
+
+public class ValuationAdapter extends RecyclerView.Adapter<ValuationAdapter.ValuationViewHolder> {
+
+    private List<Valuation> valuationList; // Danh sách sản phẩm
+    private Context context; // Context để khởi tạo Intent
+
+    // Constructor
+    public ValuationAdapter(List<Valuation> valuationList, Context context) {
+        this.valuationList = valuationList;
+        this.context = context; // Lưu context
+    }
+
+    // Tạo ViewHolder
+    @NonNull
+    @Override
+    public ValuationViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_valuation, parent, false);
+        return new ValuationViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ValuationViewHolder holder, int position) {
+        Valuation product = valuationList.get(position);
+        holder.productName.setText(product.getJewelry().getName());
+        holder.productImage.setImageResource(R.drawable.ic_complete); // Set image resource
+        holder.productPrice.setText("Starting Price: " + product.getJewelry().getStaringPrice() + " VND");
+        holder.productStatus.setText("Status: " + product.getStatus());
+//        holder.productStatus.setText(product.getStatus());
+        // Thiết lập sự kiện nhấn vào item sản phẩm
+        holder.itemView.setOnClickListener(v -> {
+            Intent intent = new Intent(context, DetailValuating.class);
+            intent.putExtra("VALUATION_ID",  product.getId());
+            context.startActivity(intent);
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return valuationList.size();
+    }
+
+    // ViewHolder cho item sản phẩm
+    public static class ValuationViewHolder extends RecyclerView.ViewHolder {
+        ImageView productImage;
+        TextView productName, productPrice, productStatus;
+
+        public ValuationViewHolder(View itemView) {
+            super(itemView);
+            productImage = itemView.findViewById(R.id.valuation_image);
+            productName = itemView.findViewById(R.id.valuation_name);
+            productPrice = itemView.findViewById(R.id.valuation_startingPrice);
+            productStatus = itemView.findViewById(R.id.valuation_status);
+        }
+    }
+}
